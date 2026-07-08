38a23b6ee9d0c22c93e6b570bcee4050b97d1279
strawberryCao <caoheming139@gmail.com>
2026-07-08 13:41:21 +0800

    Load selected wallpaper theme

--- outputs/kaoyan-schedule-app/src/App.tsx
+++ outputs/kaoyan-schedule-app/src/App.tsx
@@ -2,6 +2,7 @@ import { ScheduleApp } from './components/ScheduleApp';
 import { WallpaperView } from './components/WallpaperView';
 import './wallpaper.css';
 import './notes.css';
+import './theme-fifth.css';
 
 export default function App() {
   const isWallpaperMode = new URLSearchParams(window.location.search).get('wallpaper') === '1';
