a9b5fbb290a4d859a0226ffa78cb7b6102fcfbb4
strawberryCao <caoheming139@gmail.com>
2026-07-08 11:30:15 +0800

    Remove desktop attach API from preload

--- outputs/kaoyan-schedule-app/electron/preload.cjs
+++ outputs/kaoyan-schedule-app/electron/preload.cjs
@@ -7,7 +7,6 @@ contextBridge.exposeInMainWorld('kaoyanDesktop', {
   setAutoLaunch: (enabled) => ipcRenderer.invoke('auto-launch:set', Boolean(enabled)),
   restoreDefaultPosition: () => ipcRenderer.invoke('window:restore-default'),
   savePosition: () => ipcRenderer.invoke('window:save-position'),
-  attachToDesktop: () => ipcRenderer.invoke('window:attach-desktop'),
   minimize: () => ipcRenderer.send('window:minimize'),
   hide: () => ipcRenderer.send('window:hide'),
   close: () => ipcRenderer.send('window:close'),
