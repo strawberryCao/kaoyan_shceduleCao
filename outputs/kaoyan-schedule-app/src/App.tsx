7a74f9bcb24cf1e7bb8271376d21dc46abcc2dac
strawberryCao <caoheming139@gmail.com>
2026-07-08 14:20:42 +0800

    Add unified app hub route

--- outputs/kaoyan-schedule-app/src/App.tsx
+++ outputs/kaoyan-schedule-app/src/App.tsx
@@ -1,3 +1,4 @@
+import { AppHub } from './components/AppHub';
 import { ScheduleApp } from './components/ScheduleApp';
 import { NoteCapturePage } from './components/NoteCapturePage';
 import { DesktopConsole } from './desktop/DesktopConsole';
@@ -6,13 +7,20 @@ import './wallpaper.css';
 import './notes.css';
 import './theme-fifth.css';
 import './desktop/desktop.css';
+import './desktop/desktop-fixes.css';
 import './note-capture.css';
+import './app-hub.css';
 
 export default function App() {
   const params = new URLSearchParams(window.location.search);
   const isWallpaperMode = params.get('wallpaper') === '1';
   const isConsoleMode = params.get('console') === '1';
   const isNotesMode = params.get('notes') === '1';
+  const isHubMode = params.get('hub') === '1';
+
+  if (isHubMode) {
+    return <AppHub />;
+  }
 
   if (isNotesMode) {
     return <NoteCapturePage />;
